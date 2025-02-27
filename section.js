9ea0180bad20aeaffbc436c618f3e2cc47b8295f
Md Sakib <muhammadsakib@4l68v8.onmicrosoft.com>
2025-02-27 10:24:47 +0600

    version--3.067

--- section.js
+++ section.js
@@ -18,11 +18,11 @@ document.getElementById("add_money_btn")
     this.style.background = '#EEEEEE'
     const section1 = document.getElementById("add_money");
     section1.classList.remove("hidden");
-    const ids1 = ['cashout']
+    const ids1 = ['cashout','transfer_Money', 'get_bonus']
     ids1.forEach(id => {
       document.getElementById(id).classList.add("hidden");
     })
-    const ids2 = ['cashout_btn']
+    const ids2 = ['cashout_btn','transfer_Money_btn', 'get_bonus_btn']
     ids2.forEach(id => {
       document.getElementById(id).style.background = 'white';
     })
@@ -35,15 +35,49 @@ document.getElementById("add_money_btn")
 document.getElementById("cashout_btn")
   .addEventListener('click', function activateSection() {
     this.style.background = '#EEEEEE'
-    const ids2 = ['add_money_btn']
+    const ids2 = ['add_money_btn','transfer_Money_btn', 'get_bonus_btn']
     ids2.forEach(id => {
       document.getElementById(id).style.background = 'white';
     })
-    const ids = ['add_money']
+    const ids = ['add_money', 'transfer_Money', 'get_bonus']
     ids.forEach(id => {
       document.getElementById(id).classList.add("hidden");
     })
     const section2 = document.getElementById("cashout");
+    section2.classList.remove("hidden");
+    const tSection = document.getElementById('footer_section')
+    tSection.classList.remove('hidden')
+  })
+  // TM
+document.getElementById("transfer_Money_btn")
+  .addEventListener('click', function activateSection() {
+    this.style.background = '#EEEEEE'
+    const ids2 = ['add_money_btn','cashout_btn', 'get_bonus_btn']
+    ids2.forEach(id => {
+      document.getElementById(id).style.background = 'white';
+    })
+    const ids = ['add_money', 'cashout', 'get_bonus']
+    ids.forEach(id => {
+      document.getElementById(id).classList.add("hidden");
+    })
+    const section2 = document.getElementById("transfer_Money");
+    section2.classList.remove("hidden");
+    const tSection = document.getElementById('footer_section')
+    tSection.classList.remove('hidden')
+  })
+  // GB
+document.getElementById("get_bonus_btn")
+  .addEventListener('click', function activateSection() {
+    this.style.background = '#EEEEEE'
+    const ids2 = ['add_money_btn','cashout_btn', 'transfer_Money_btn']
+    ids2.forEach(id => {
+      document.getElementById(id).style.background = 'white';
+    })
+    const ids = ['add_money', 'cashout', 'transfer_Money']
+    ids.forEach(id => {
+      document.getElementById(id).classList.add("hidden");
+    })
+    const section2 = document.getElementById("get_bonus");
     section2.classList.remove("hidden");
     const tSection = document.getElementById('footer_section')
     tSection.classList.remove('hidden')
